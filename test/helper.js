a0612fd6fde4671802e4ba1704c85b6d16244487
Josh Duff <me@JoshDuff.com>
2017-07-13 23:38:28 -0500

    Moving test helper to test/

--- test/helper.js
+++ test/helper.js
@@ -1,21 +1,21 @@
-const httpGet = require('./get.js')
+const httpGet = require('../get.js')
 const buildIndexUrl = key => `https://spreadsheets.google.com/feeds/worksheets/${key}/public/basic?alt=json`
 const buildTableUrl = (key, sheetId) => `https://spreadsheets.google.com/feeds/list/${key}/${sheetId}/public/values?alt=json`
 const fs = require('fs')
 
-const testCases = require('./test/cases.js')
+const testCases = require('./cases.js')
 
 function outputSheet(key, sheetId) {
 	httpGet(buildTableUrl(key, sheetId)).then(body => {
 		const json = JSON.stringify(body, null, '\t')
-		fs.writeFileSync(`test/fixture-${key}-sheet-${sheetId}.json`, json)
+		fs.writeFileSync(`./fixture-${key}-sheet-${sheetId}.json`, json)
 	})
 }
 
 function outputIndex(key) {
 	httpGet(buildIndexUrl(key)).then(body => {
 		const json = JSON.stringify(body, null, '\t')
-		fs.writeFileSync(`test/fixture-${key}.json`, json)
+		fs.writeFileSync(`./fixture-${key}.json`, json)
 	})
 }
 
