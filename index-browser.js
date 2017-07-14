e067d160d6f40d8aeef61c2e2f0ddc9f275e58e2
Josh Duff <me@JoshDuff.com>
2017-07-14 13:40:29 -0500

    non-200/300 errors are errors (got does this already)

--- index-browser.js
+++ index-browser.js
@@ -1,13 +1,25 @@
 const createApi = require('./index.js')
 
+const success = request => request.status >= 200 && request.status < 400
+
 function defaultGet(url) {
 	return new Promise((resolve, reject) => {
 		const request = new XMLHttpRequest()
-		request.addEventListener('load', () => resolve(JSON.parse(request.responseText)))
+		request.addEventListener('load', handleResult)
 		request.addEventListener('error', reject)
 		request.addEventListener('abort', reject)
 		request.open('GET', url)
 		request.send()
+
+		function handleResult() {
+			try {
+				const response = JSON.parse(request.responseText)
+
+				success(request) ? resolve(response) : reject(response)
+			} catch (e) {
+				reject('Invalid response: ' + request.responseText)
+			}
+		}
 	})
 }
 
