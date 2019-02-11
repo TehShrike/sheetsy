ca3a161765686982cc809ecf52ead9ce29f41632
Elisarmor <ejsisgreat@gmail.com>
2019-02-11 14:57:35 -0600

    Make getSheet return false when there are no entries

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ module.exports = defaultGet => {
 	function getSheet(key, id, get = defaultGet) {
 		return get(buildSheetUrl(key, id)).then(sheetData => {
 			const feed = sheetData.feed
+			if (feed.entry === undefined) return false;
 			const rows = feed.entry.map(entry => {
 				const originalCellKeysAndValues = entries(entry)
 					.filter(([ key ]) => /^gsx\$/.test(key))
